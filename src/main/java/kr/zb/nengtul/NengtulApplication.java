9baf7ea38dbc70993caf4e55ec5f2342ac33a82f
IamJaehyun <lvet0330@gmail.com>
2023-07-15 04:09:12 +0900

    feat : setting
    revert 잘못해서 다시 세팅..
    초기세팅
    부트 3.1.1
    자바 17
    그래들 8.1.1

---
+++ src/main/java/kr/zb/nengtul/NengtulApplication.java
@@ -0,0 +1,13 @@
+package kr.zb.nengtul;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NengtulApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NengtulApplication.class, args);
+	}
+
+}
---
+++ src/test/java/kr/zb/nengtul/NengtulApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.zb.nengtul;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NengtulApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
