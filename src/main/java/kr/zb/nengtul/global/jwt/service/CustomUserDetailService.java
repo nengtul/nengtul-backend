05a70f5b67f79db363df346be8feea72b8cd9d0d
IamJaehyun <lvet0330@gmail.com>
2023-07-17 16:00:01 +0900

    feat : jwt Token with refreshtoken
    jwt 토큰 및 refresh 토큰 구현
    로그인시에 자동으로 헤더에 담기도록 구현
    login default URL : /v1/nengtul/user/login

--- src/main/java/kr/zb/nengtul/global/jwt/service/CustomUserDetailService.java
+++ src/main/java/kr/zb/nengtul/global/jwt/service/CustomUserDetailService.java
@@ -2,25 +2,27 @@ package kr.zb.nengtul.global.jwt.service;
 
 import kr.zb.nengtul.user.entity.domain.User;
 import kr.zb.nengtul.user.entity.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 public class CustomUserDetailService implements UserDetailsService {
 
   private final UserRepository userRepository;
 
-  public CustomUserDetailService(UserRepository userRepository) {
-    this.userRepository = userRepository;
-  }
-
   @Override
   public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
     User user = userRepository.findByEmail(email)
-        .orElseThrow(() -> new UsernameNotFoundException("사용자를 찾을 수 없습니다."));
+        .orElseThrow(() -> new UsernameNotFoundException("해당 이메일이 존재하지 않습니다."));
 
-    return new JwtUserDetails(user);
+    return org.springframework.security.core.userdetails.User.builder()
+        .username(user.getEmail())
+        .password(user.getPassword())
+        .roles(user.getRoles().name())
+        .build();
   }
 }
