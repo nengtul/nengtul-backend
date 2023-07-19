2905d6a5e9b02da011699208f3d687cd0cc9661b
IamJaehyun <lvet0330@gmail.com>
2023-07-19 18:36:36 +0900

    feat : updateUser
    유저 업데이트 로직 구현 및 update 시 상황에 따른 에러 적용

--- src/main/java/kr/zb/nengtul/user/controller/UserController.java
+++ src/main/java/kr/zb/nengtul/user/controller/UserController.java
@@ -5,12 +5,14 @@ import java.util.Map;
 import kr.zb.nengtul.global.jwt.JwtToken;
 import kr.zb.nengtul.user.entity.dto.UserJoinDto;
 import kr.zb.nengtul.user.entity.dto.UserLoginDto;
+import kr.zb.nengtul.user.entity.dto.UserUpdateDto;
 import kr.zb.nengtul.user.service.UserService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.crossstore.ChangeSetPersister.NotFoundException;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -28,16 +30,15 @@ public class UserController {
     return ResponseEntity.ok(userService.join(userJoinDto));
   }
 
-//  @PostMapping("/login")
-//  public ResponseEntity<String> login(@RequestBody UserLoginDto userLoginDto) throws Exception {
-//
-//    return ResponseEntity.ok(userService.login(userLoginDto));
-//  }
-
   @PostMapping("/quit")
   public ResponseEntity<Void> quit(Principal principal){
     userService.quit(principal);
     return ResponseEntity.ok(null);
+  }
+
+  @PutMapping("/update")
+  public ResponseEntity<String> update(Principal principal,@RequestBody UserUpdateDto userUpdateDto){
+    return ResponseEntity.ok(userService.update(principal, userUpdateDto));
   }
 
 }
