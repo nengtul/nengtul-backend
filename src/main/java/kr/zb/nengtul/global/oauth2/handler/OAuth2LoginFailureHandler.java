2027354456cd3c3b31872d2ee931e86b5b7826bb
IamJaehyun <lvet0330@gmail.com>
2023-07-19 18:12:27 +0900

    fix : fix ErrorHandler
    에러 핸들러 수정

--- src/main/java/kr/zb/nengtul/global/oauth2/handler/OAuth2LoginFailureHandler.java
+++ src/main/java/kr/zb/nengtul/global/oauth2/handler/OAuth2LoginFailureHandler.java
@@ -1,14 +1,16 @@
 package kr.zb.nengtul.global.oauth2.handler;
 
 
+import static kr.zb.nengtul.global.exception.ErrorCode.CHECK_SOCIAL_SERVER;
+
 import jakarta.servlet.ServletException;
-import jakarta.servlet.http.*;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.authentication.AuthenticationFailureHandler;
 import org.springframework.stereotype.Component;
-
-import java.io.IOException;
 
 @Slf4j
 @Component
@@ -17,8 +19,15 @@ public class OAuth2LoginFailureHandler implements AuthenticationFailureHandler {
   @Override
   public void onAuthenticationFailure(HttpServletRequest request, HttpServletResponse response,
       AuthenticationException exception) throws IOException, ServletException {
-    response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
-    response.getWriter().write("소셜 로그인 실패! 서버 로그를 확인해주세요.");
+    response.setStatus(CHECK_SOCIAL_SERVER.getHttpStatus().value());
+    response.setCharacterEncoding("UTF-8");
+    response.setContentType("application/json;charset=UTF-8");
+
+    String errorMessage = "{\"status\": " + CHECK_SOCIAL_SERVER.getHttpStatus().value() +
+        ", \"code\": \"" + "CHECK_ID_AND_PW" +
+        "\", \"message\": \"" + CHECK_SOCIAL_SERVER.getDetail() + "\"}";
+
+    response.getWriter().write(errorMessage);
     log.info("소셜 로그인에 실패했습니다. 에러 메시지 : {}", exception.getMessage());
   }
 }
