fc0d472906be2e86e6bfb427e84e6f3ca29cd263
IamJaehyun <lvet0330@gmail.com>
2023-07-19 18:12:09 +0900

    feat : define exception
    CustomException 정의

---
+++ src/main/java/kr/zb/nengtul/global/exception/ApiExceptionAdvice.java
@@ -0,0 +1,23 @@
+package kr.zb.nengtul.global.exception;
+
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+
+@RestControllerAdvice
+public class ApiExceptionAdvice {
+  // CustomException 이 발생했을 때 처리하는 핸들러 메서드
+  @ExceptionHandler({CustomException.class})
+  public ResponseEntity<CustomException.CustomExceptionResponse> exceptionHandler(
+      HttpServletRequest req, final CustomException e){
+    // CustomExceptionResponse 객체를 생성하여 응답 반환
+    return ResponseEntity
+        .status(e.getStatus())
+        .body(CustomException.CustomExceptionResponse.builder()
+            .message(e.getMessage())
+            .code(e.getErrorCode().name())
+            .status(e.getStatus()).build());
+  }
+}
