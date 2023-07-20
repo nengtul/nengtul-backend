406c092496b96f9920ae7a65141542bb4137405a
IamJaehyun <lvet0330@gmail.com>
2023-07-20 19:17:30 +0900

    feat : findEmail & findpw
    이메일(아이디) 찾기와 비밀번호 찾기(Email 을 통한 임시비밀번호 발급) 구현 완료

--- src/main/java/kr/zb/nengtul/user/entity/repository/UserRepository.java
+++ src/main/java/kr/zb/nengtul/user/entity/repository/UserRepository.java
@@ -7,6 +7,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User, Long> {
   Optional<User> findByEmail(String email);
+  Optional<User> findByName(String name);
   Optional<User> findByRefreshToken(String refreshToken);
   Optional<User> findByProviderTypeAndSocialId(ProviderType providerType, String socialId);
   boolean existsByEmail(String email);
