115aef2d74718d36c729613be189598045357a02
IamJaehyun <lvet0330@gmail.com>
2023-07-22 15:31:44 +0900

    fix : userService
    중복 코드 추출 및 단순 String return 값 void 로 변경

--- src/main/java/kr/zb/nengtul/user/controller/UserController.java
+++ src/main/java/kr/zb/nengtul/user/controller/UserController.java
@@ -31,22 +31,23 @@ public class UserController {
 
   //회원가입
   @PostMapping("/join")
-  public ResponseEntity<String> join(@RequestBody UserJoinDto userJoinDto) {
-    return ResponseEntity.ok(userService.join(userJoinDto));
+  public ResponseEntity<Void> join(@RequestBody UserJoinDto userJoinDto) {
+    userService.join(userJoinDto);
+    return ResponseEntity.ok(null);
   }
 
   //이메일 인증 (이메일에서 링크를 클릭하여 put 요청을 보낼 수 없어서 GET요청으로 처리)
   @GetMapping("/verify")
-  public ResponseEntity<String> verify(@RequestParam String email, @RequestParam String code) {
+  public ResponseEntity<Void> verify(@RequestParam String email, @RequestParam String code) {
     userService.verify(email, code);
-    return ResponseEntity.ok("인증이 완료되었습니다.");
+    return ResponseEntity.ok(null);
   }
 
   //이메일 인증번호 재발급 요청
   @PostMapping("/verify/reset/{userId}")
-  public ResponseEntity<String> resetVerify(@PathVariable Long userId) {
+  public ResponseEntity<Void> resetVerify(@PathVariable Long userId) {
     userService.resetVerify(userId);
-    return ResponseEntity.ok("코드 재발급 및 연장이 완료되었습니다.");
+    return ResponseEntity.ok(null);
   }
 
   //아이디 찾기
@@ -57,8 +58,9 @@ public class UserController {
 
   //임시 비밀번호 발급
   @GetMapping("/findpw")
-  public ResponseEntity<String> getNewPassword(@RequestBody UserFindPasswordDto userFindPasswordDto) {
-    return ResponseEntity.ok(userService.getNewPassword(userFindPasswordDto));
+  public ResponseEntity<Void> getNewPassword(@RequestBody UserFindPasswordDto userFindPasswordDto) {
+    userService.getNewPassword(userFindPasswordDto);
+    return ResponseEntity.ok(null);
   }
 
   //회원 상세보기
