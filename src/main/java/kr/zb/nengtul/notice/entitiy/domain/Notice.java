90bf2b0a7e734e7a377ec3e52e5b1d8b34b484b9
IamJaehyun <lvet0330@gmail.com>
2023-07-23 21:19:02 +0900

    feat : Notice
    Notice 정의 (User : Notice 관계 1:M 정의)
    
    순환참조 방지하기위해 JsonManagedReference / JsonBackReference 사용 (Response Dto 사용)
    
    SecurityConfig 에 notice Admin 권한일때만 사용 가능하도록 변경
    USER 권한일 때 할 수 있는 모든 것 ADMIN  권한일때도 가능하도록 변경

---
+++ src/main/java/kr/zb/nengtul/notice/entitiy/domain/Notice.java
@@ -0,0 +1,41 @@
+package kr.zb.nengtul.notice.entitiy.domain;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonManagedReference;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import kr.zb.nengtul.global.entity.BaseTimeEntity;
+import kr.zb.nengtul.user.entity.domain.User;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Builder
+public class Notice extends BaseTimeEntity {
+  @JsonIgnore
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+
+  @JsonManagedReference
+  @ManyToOne(fetch = FetchType.LAZY)
+  private User user;
+
+  private String title;
+
+  private String content;
+
+  private String noticeImg;
+
+  private Long viewCount;
+}
---
+++ src/main/java/kr/zb/nengtul/notice/entitiy/repository/NoticeRepository.java
@@ -0,0 +1,8 @@
+package kr.zb.nengtul.notice.entitiy.repository;
+
+import kr.zb.nengtul.notice.entitiy.domain.Notice;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface NoticeRepository extends JpaRepository<Notice, Long> {
+
+}
