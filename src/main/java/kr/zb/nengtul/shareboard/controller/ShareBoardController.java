7e638d98b94c1856d09fcdd020712551170fc83e
IamJaehyun <lvet0330@gmail.com>
2023-07-26 20:53:53 +0900

    feat : get shareBoard list
    DeleteMapping 이 Put으로 되어있어서 수정
    
    나눔게시판 조회 요청시 lat+-범위값 / lon+-범위값 에 해당하는 모든 요청을 list 로 반환
    
    closed 추가(거래여부)
    
    - 거래 완료, 거래 가능 두가지 모두 조회 
    /v1/shareboard?lat=1.24363456&lon=1.53415245&range=0.3
    - 거래 가능 게시물만 조회 
    /v1/shareboard?lat=1.24363456&lon=1.53415245&range=0.3&closed=false
    - 거래 완료 게시물만 조회 
    /v1/shareboard?lat=1.24363456&lon=1.53415245&range=0.3&closed=true
    
    나눔게시판 조회수 삭제
    
    위도 경도 null 불가, 범위 validation 코드 지정
    
    나눔완료/구매완료 처리는 추후에 채팅방 내에서 할 수 있도록 할 생각

--- src/main/java/kr/zb/nengtul/shareboard/controller/ShareBoardController.java
+++ src/main/java/kr/zb/nengtul/shareboard/controller/ShareBoardController.java
@@ -2,16 +2,21 @@ package kr.zb.nengtul.shareboard.controller;
 
 import jakarta.validation.Valid;
 import java.security.Principal;
+import java.util.List;
 import kr.zb.nengtul.shareboard.domain.dto.ShareBoardDto;
+import kr.zb.nengtul.shareboard.domain.dto.ShareBoardListDto;
 import kr.zb.nengtul.shareboard.service.ShareBoardService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -19,7 +24,9 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 @RequestMapping("/v1/shareboard")
 public class ShareBoardController {
+
   private final ShareBoardService shareBoardService;
+
   //생성
   @PostMapping
   public ResponseEntity<Void> create(@RequestBody @Valid ShareBoardDto shareBoardDto,
@@ -28,16 +35,25 @@ public class ShareBoardController {
     return ResponseEntity.ok(null);
   }
 
+  //전체 조회
+  @GetMapping
+  public ResponseEntity<List<ShareBoardListDto>> getList(@RequestParam double lat,
+      @RequestParam double lon, @RequestParam double range,
+      @RequestParam(required = false) Boolean closed) {
+    return ResponseEntity.ok(shareBoardService.getList(lat, lon, range, closed));
+  }
+
   //수정
   @PutMapping("/{id}")
-  public ResponseEntity<Void> create(@PathVariable Long id, @RequestBody @Valid ShareBoardDto shareBoardDto,
+  public ResponseEntity<Void> create(@PathVariable Long id,
+      @RequestBody @Valid ShareBoardDto shareBoardDto,
       Principal principal) {
-    shareBoardService.update(id,shareBoardDto, principal);
+    shareBoardService.update(id, shareBoardDto, principal);
     return ResponseEntity.ok(null);
   }
 
   //삭제
-  @PutMapping("/{id}")
+  @DeleteMapping("/{id}")
   public ResponseEntity<Void> delete(@PathVariable Long id, Principal principal) {
     shareBoardService.delete(id, principal);
     return ResponseEntity.ok(null);
--- src/main/java/kr/zb/nengtul/shareboard/domain/entity/ShareBoard.java
+++ src/main/java/kr/zb/nengtul/shareboard/domain/entity/ShareBoard.java
@@ -6,7 +6,6 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import kr.zb.nengtul.global.entity.BaseTimeEntity;
 import kr.zb.nengtul.user.domain.entity.User;
@@ -35,15 +34,12 @@ public class ShareBoard extends BaseTimeEntity {
 
   private String shareImg;
 
-  private String content;
-
   private Long price;
 
   private double lat;//위도
 
   private double lon;//경도
 
-  @Column(columnDefinition = "bigint default 0", nullable = false)
-  private Long views;
-
+  @Column(columnDefinition = "boolean default false", nullable = false)
+  private boolean closed; //거래 완료 여부
 }
