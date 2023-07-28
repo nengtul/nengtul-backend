e1d65b3c4dadadbac1d4e9bfc1a34846dd541652
Kang Sung-hyeok <rkdtjd976@gmail.com>
2023-07-28 15:03:50 +0900

    ### 변경사항 **AS-IS** http 요청 시 security pattern 오류 발생하여 수정

--- src/main/java/kr/zb/nengtul/global/config/SecurityConfig.java
+++ src/main/java/kr/zb/nengtul/global/config/SecurityConfig.java
@@ -59,7 +59,7 @@ public class SecurityConfig {
 
         //== URL별 권한 관리 옵션 ==//
         .authorizeHttpRequests(auth -> auth
-            .requestMatchers("/", "/css/**", "/images/**", "/js/**", "/favicon.ico","/**/favicon.ico",
+            .requestMatchers("/", "/css/**", "/images/**", "/js/**", "/favicon.ico",
                 "/h2-console/**",
                 "/index.html",
                 "/login/**",
