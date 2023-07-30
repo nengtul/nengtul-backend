905ee4e5e68cf3d13f5a458f540816fd8dda5733
NokchaMat <rkdtjd976@gmail.com>
2023-07-30 19:40:57 +0900

    젠킨스에서 gradlew 빌드 시 s3bucket의 경로를 찾지 못하는 이슈 - root 폴더의 kr.zb의 경로가 같아 충돌로 인하여 못 찾는 것으로 예상. s3bucket의 경로에 kr.zb 삭제
    - bean create 이슈 발생하여 ComponentScan으로 지정

--- src/main/java/kr/zb/nengtul/recipe/service/RecipeService.java
+++ src/main/java/kr/zb/nengtul/recipe/service/RecipeService.java
@@ -9,7 +9,6 @@ import kr.zb.nengtul.recipe.domain.dto.RecipeGetListDto;
 import kr.zb.nengtul.recipe.domain.dto.RecipeUpdateDto;
 import kr.zb.nengtul.recipe.domain.entity.RecipeDocument;
 import kr.zb.nengtul.recipe.domain.repository.RecipeSearchRepository;
-import kr.zb.nengtul.s3bucket.service.AmazonS3Service;
 import kr.zb.nengtul.user.domain.entity.User;
 import kr.zb.nengtul.user.domain.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
@@ -23,6 +22,7 @@ import java.security.Principal;
 import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Objects;
+import s3bucket.service.AmazonS3Service;
 
 @Service
 @RequiredArgsConstructor
