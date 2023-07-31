b88a9f7d713f38353cacf0e4a8a1a1ef906dd876
Nokchamat <107979129+Nokchamat@users.noreply.github.com>
2023-07-31 23:39:07 +0900

    Update NengtulApplication.java

--- src/main/java/kr/zb/nengtul/NengtulApplication.java
+++ src/main/java/kr/zb/nengtul/NengtulApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.ImportAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.cloud.openfeign.FeignAutoConfiguration;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.scheduling.annotation.EnableScheduling;
