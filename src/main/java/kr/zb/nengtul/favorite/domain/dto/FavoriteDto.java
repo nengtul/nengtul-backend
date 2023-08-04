dfaec7c5c53ad727ba22b4b0b912454cb13dc959
Kang Sung-hyeok <rkdtjd976@gmail.com>
2023-08-04 19:32:36 +0900

    즐겨찾기 CRUD 구현 완료

---
+++ src/main/java/kr/zb/nengtul/favorite/domain/dto/FavoriteDto.java
@@ -0,0 +1,18 @@
+package kr.zb.nengtul.favorite.domain.dto;
+
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Builder
+public class FavoriteDto {
+
+    private Long id;
+
+    private Long publisherId;
+
+    private String publisherNickName;
+
+    private String publisherProfilePhotoUrl;
+
+}
