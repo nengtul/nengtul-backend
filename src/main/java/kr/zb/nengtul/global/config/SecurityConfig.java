568ddf289c37133b192c57f6e6f43d2071f33398
zxcv5595 <zxcv5595@korea.ac.kr>
2023-08-10 15:18:31 +0900

    chore/chat : merge conflict 해결

--- src/main/java/kr/zb/nengtul/global/config/SecurityConfig.java
+++ src/main/java/kr/zb/nengtul/global/config/SecurityConfig.java
@@ -5,6 +5,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import kr.zb.nengtul.global.filter.CustomJsonUsernamePasswordAuthenticationFilter;
 import kr.zb.nengtul.global.handler.LoginFailureHandler;
 import kr.zb.nengtul.global.handler.LoginSuccessHandler;
+import kr.zb.nengtul.global.handler.TokenAccessDeniedHandler;
 import kr.zb.nengtul.global.jwt.JwtAuthenticationProcessingFilter;
 import kr.zb.nengtul.global.jwt.JwtTokenProvider;
 import kr.zb.nengtul.global.jwt.service.CustomUserDetailService;
@@ -17,6 +18,10 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.access.expression.method.DefaultMethodSecurityExpressionHandler;
+import org.springframework.security.access.expression.method.MethodSecurityExpressionHandler;
+import org.springframework.security.access.hierarchicalroles.RoleHierarchy;
+import org.springframework.security.access.hierarchicalroles.RoleHierarchyImpl;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.ProviderManager;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
@@ -47,6 +52,7 @@ public class SecurityConfig {
     private final OAuth2LoginSuccessHandler oAuth2LoginSuccessHandler;
     private final OAuth2LoginFailureHandler oAuth2LoginFailureHandler;
     private final CustomOAuth2UserService customOAuth2UserService;
+    private final TokenAccessDeniedHandler tokenAccessDeniedHandler;
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
@@ -72,12 +78,14 @@ public class SecurityConfig {
                         .requestMatchers(HttpMethod.GET, "/v1/notices/**").permitAll()//공지사항 조회
                         .requestMatchers(// -- Swagger UI v2
                                 "/v2/api-docs",
-                                "/swagger-resources", "/swagger-resources/**", "/configuration/ui",
+                                "/swagger-resources",
+                                "/swagger-resources/**",
+                                "/configuration/ui",
                                 "/configuration/security",
                                 "/swagger-ui.html",
                                 "/webjars/**",
+                                "/v3/api-docs/**",
                                 // -- Swagger UI v3 (OpenAPI)"/v3/api-docs/**",
-
                                 "/swagger-ui/**").permitAll()
                         .requestMatchers(
                                 "/", "/**",
@@ -87,19 +95,19 @@ public class SecurityConfig {
                                 "/v1/users/findpw",//비밀번호 찾기 (비밀번호 재발급)
                                 "/v1/users/findid",//아이디 찾기
                                 "/v1/users/verify/**", //이메일 인증
-
                                 "/v1/recipe/commentlist/**",//댓글 조회
                                 "/chat/**"
                         ).permitAll()
-                        .requestMatchers("/v1/users/**",
+                        .requestMatchers(
+                                "/v1/users/**",
                                 "/v1/shareboards/**",
                                 "/v1/recipe/**",
                                 "/v1/comments/**",//대댓글
                                 "/v1/recipes/comment/**",//댓글 작성,수정,삭제
-                                "/v1/chat/**",
                                 "/v1/likes/**",
                                 "/v1/favorite/**",
-                                "/v1/saved-recipe/**"
+                                "/v1/saved-recipe/**",
+                                "/v1/chat/**"
                         ).hasRole("USER")
                         .anyRequest().authenticated()) // 위의 경로 이외에는 모두 인증된 사용자만 접근 가능
                 .logout(logout -> logout.logoutSuccessUrl("/"))
@@ -169,30 +177,31 @@ public class SecurityConfig {
                 jwtTokenProvider, userRepository);
     }
 
-  //cors 설정
-  @Bean
-  public UrlBasedCorsConfigurationSource corsConfigurationSource() {
-    CorsConfiguration configuration = new CorsConfiguration();
-    configuration.addAllowedOriginPattern("*");
-    configuration.addAllowedMethod("*");
-    configuration.addAllowedHeader("*");
-    configuration.setAllowCredentials(true);
-    UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-    source.registerCorsConfiguration("/**", configuration);
-    return source;
-  }
+    //cors 설정
+    @Bean
+    public UrlBasedCorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.addAllowedOriginPattern("*");
+        configuration.addAllowedMethod("*");
+        configuration.addAllowedHeader("*");
+        configuration.setAllowCredentials(true);
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
+    }
 
-  @Bean
-  public RoleHierarchy roleHierarchy() {
-    RoleHierarchyImpl roleHierarchy = new RoleHierarchyImpl();
-    roleHierarchy.setHierarchy("ROLE_ADMIN > ROLE_USER");
-    return roleHierarchy;
-  }
+    @Bean
+    public RoleHierarchy roleHierarchy() {
+        RoleHierarchyImpl roleHierarchy = new RoleHierarchyImpl();
+        roleHierarchy.setHierarchy("ROLE_ADMIN > ROLE_USER");
+        return roleHierarchy;
+    }
 
-  @Bean
-  public MethodSecurityExpressionHandler methodSecurityExpressionHandler(RoleHierarchy roleHierarchy) {
-    DefaultMethodSecurityExpressionHandler expressionHandler = new DefaultMethodSecurityExpressionHandler();
-    expressionHandler.setRoleHierarchy(roleHierarchy);
-    return expressionHandler;
-  }
+    @Bean
+    public MethodSecurityExpressionHandler methodSecurityExpressionHandler(
+            RoleHierarchy roleHierarchy) {
+        DefaultMethodSecurityExpressionHandler expressionHandler = new DefaultMethodSecurityExpressionHandler();
+        expressionHandler.setRoleHierarchy(roleHierarchy);
+        return expressionHandler;
+    }
 }
