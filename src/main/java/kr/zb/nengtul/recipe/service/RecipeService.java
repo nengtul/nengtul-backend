d450ea738d9669a5458b260f2be3d51f0fac27aa
Kang Sung-hyeok <rkdtjd976@gmail.com>
2023-08-10 19:28:42 +0900

    Recipe Category --- - 샐러드 오탈자 수정 - 양식 Enum 값 추가

--- src/main/java/kr/zb/nengtul/recipe/service/RecipeService.java
+++ src/main/java/kr/zb/nengtul/recipe/service/RecipeService.java
@@ -207,7 +207,6 @@ public class RecipeService {
                         .build());
 
         Long likeCount = likesRepository.countByRecipeId(recipeDocument.getId());
-        System.out.println(likeCount);
 
         recipeGetListDto.setNickName(user.getNickname());
         recipeGetListDto.setLikeCount(likeCount);
